4c5e542b4077bb9e207e18513a5e3af0b53b7564
categorille <categorille@protonmail.com>
2021-03-31 18:30:21 +0200

    moved terminal context to struct

--- kernel.c
+++ kernel.c
@@ -29,15 +29,15 @@ enum vga_color {
     VGA_COLOR_WHITE         = 15,
 };
 
-static inline uint8_t vga_entry_color(const enum vga_color fg, const enum vga_color bg) {
+static inline uint8_t vga_entry_color(enum vga_color const fg, enum vga_color const bg) {
     return fg | bg << 4;
 }
 
-static inline uint16_t vga_entry(const unsigned char uc, const uint8_t color) {
+static inline uint16_t vga_entry(unsigned char const uc, uint8_t const color) {
     return (uint16_t) uc | (uint16_t) color << 8;
 }
 
-size_t strlen(const char const* str) {
+size_t str_len(char const *const str) {
     size_t len = 0;
     while (str[len]) {
         len++;
@@ -45,59 +45,81 @@ size_t strlen(const char const* str) {
     return len;
 }
 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
+static size_t const VGA_WIDTH = 80;
+static size_t const VGA_HEIGHT = 25;
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
+struct term_ctx {
+    size_t row;
+    size_t column;
+    uint8_t color;
+};
 
-void terminal_initialize(void) {
-    terminal_row = 0;
-    terminal_column = 0;
-    terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-    terminal_buffer = (uint16_t*) 0xb8000;
+void term_ctx_init(struct term_ctx *ctx) {
+    ctx->column = 0;
+    ctx->row = 0;
+    ctx->color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+}
+
+uint16_t* term_buffer;
+
+void term_init(struct term_ctx *ctx) {
+    term_ctx_init(ctx);
+    term_buffer = (uint16_t*) 0xb8000;
     for (size_t y = 0; y < VGA_HEIGHT; ++y) {
         for (size_t x = 0; x < VGA_WIDTH; ++x) {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = vga_entry(' ', terminal_color);
+            size_t const index = y * VGA_WIDTH + x;
+            term_buffer[index] = vga_entry(' ', ctx->color);
         }
     }
 }
 
-void terminal_setcolor(const uint8_t color) {
-    terminal_color = color;
+void term_ctx_set_color(struct term_ctx *ctx, uint8_t const color) {
+    ctx->color = color;
 }
 
-void terminal_putentryat(const char c, const uint8_t color, const size_t x, const size_t y) {
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = vga_entry(c, color);
+void term_put_entry_at(char const c, uint8_t const color, size_t const x, size_t const y) {
+    size_t const index = y * VGA_WIDTH + x;
+    term_buffer[index] = vga_entry(c, color);
 }
 
-void terminal_putchar(char c) {
-    terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    ++terminal_column;
-    if (terminal_column >= VGA_WIDTH) {
-        terminal_column = 0;
-        ++terminal_row;
-        if (terminal_row >= VGA_HEIGHT) {
-            terminal_row = 0;
+void term_put_lf(struct term_ctx *ctx) {
+    ++ctx->row;
+    if (ctx->row >= VGA_HEIGHT) {
+        ctx->row = 0;
+    }
+    ctx->column = 0;
+}
+
+void term_put_char(struct term_ctx *ctx, char c) {
+    switch (c) { // handle non-print chars
+    case '\n': 
+        term_put_lf(ctx);
+        break;
+    default:
+        term_put_entry_at(c, ctx->color, ctx->column, ctx->row);
+        ++ctx->column;
+    }
+    if (ctx->column >= VGA_WIDTH) {
+        ctx->column = 0;
+        ++ctx->row;
+        if (ctx->row >= VGA_HEIGHT) {
+            ctx->row = 0;
         }
     }
 }
 
-void terminal_write(const char const* data, const size_t size) {
+void term_write(struct term_ctx *ctx, char const *const data, size_t const size) {
     for (size_t i = 0; i < size; ++i) {
-        terminal_putchar(data[i]);
+        term_put_char(ctx, data[i]);
     }
 }
 
-void terminal_writestring(const char const* str) {
-    terminal_write(str, strlen(str));
+void term_writestring(struct term_ctx *ctx, char const *const str) {
+    term_write(ctx, str, str_len(str));
 }
 
 void kernel_main(void) {
-    terminal_initialize();
-    terminal_writestring("Hello, kernel world!");
+    struct term_ctx ctx;
+    term_init(&ctx);
+    term_writestring(&ctx, "Hello,\nkernel world!\n");
 }
